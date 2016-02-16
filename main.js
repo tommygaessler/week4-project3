1d99d0f3f44495ca85af5487b67f128dd325be2e
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-02-15 22:12:43 -0700

    finished

--- main.js
+++ main.js
@@ -1,43 +1,60 @@
 $(document).ready(function(){
-// 1. The text under "Click for short bio" should not be visible unless the user clicks the flip id.
-//    Hint: Google the jQuery slideToggle function
+	// 1. The text under "Click for short bio" should not be visible unless the user clicks the flip id.
+	//    Hint: Google the jQuery slideToggle function
+
+	$("#flip").click(function(){
+	  $("#panel").slideToggle()
+	})
+
+	// 2. When hovering over the h4 tags (Objective, Experience, etc..), 
+	//    the inside of the dotted box should turn light blue and remain light blue.
+
+	$("h4").hover(function(){
+		$(this).css("background-color", "blue");
+	});
+
+
+	// 3. When any of the project images is clicked, it should expand to 300px wide.
+
+	$(".project-image").click(function(){
+		$(this).css("width", "300px")
+	});
 
 
 
+	// 4. When clicking the "Alternate Color Theme" button, it should toggle the "changeTextColors" class.
+	//    Additionally, the h4 tag hover that you created in #2 should turn white.
+	//    Also, the background should change to the dark_embroidery.png image located the images directory.
 
-// 2. When hovering over the h4 tags (Objective, Experience, etc..), 
-//    the inside of the dotted box should turn light blue and remain light blue.
-
-
-
-
-// 3. When any of the project images is clicked, it should expand to 300px wide.
-
-
-
-
-
-// 4. When clicking the "Alternate Color Theme" button, it should toggle the "changeTextColors" class.
-//    Additionally, the h4 tag hover that you created in #2 should turn white.
-//    Also, the background should change to the dark_embroidery.png image located the images directory.
+	$("#low-vision").click(function() {
+	  $("body").toggleClass('changeTextColors');
+	    $("h4").hover(function(){
+	      $(this).css("background-color", "white");
+	    });
+	    $("body").css("background", "url('images/dark_embroidery.png')")
+	});
 
 
+	// 5. When hovering over the Twitter, Instagram, and LinkedIn buttons, the background behind the buttons should turn #6bd2db.
+	//    This has not been loaded into a CSS class and must be done within the function.
+	//    Make sure you get those corners rounded so the color doesn't pop out in the corners!
+	 
+	$("#social-links a img").hover(function() {
+	  $(this).css("background", "#6bd2db")
+	  $(this).css("border-radius", "15px")
+	});
 
+	//BONUS
+	//       The contact area contains a span that can be clicked to reveal the contact info. 
+	//       The HTML and CSS have been written for you to enable this functionality with jQuery.  
+	//       The user should click a button next to the person's name to reveal the phone number and email address.
 
-// 5. When hovering over the Twitter, Instagram, and LinkedIn buttons, the background behind the buttons should turn #6bd2db.
-//    This has not been loaded into a CSS class and must be done within the function.
-//    Make sure you get those corners rounded so the color doesn't pop out in the corners!
-  
-  
-  
-
-//BONUS
-//       The contact area contains a span that can be clicked to reveal the contact info. 
-//       The HTML and CSS have been written for you to enable this functionality with jQuery.  
-//       The user should click a button next to the person's name to reveal the phone number and email address.
+	 $("<button class='contact'>reveal</button>").insertAfter('h4:last')
+	// $("span.reference-contact").hide();
+	$("button.contact").click(function() {
+		$("span.reference-contact").toggleClass('hidden');
+	});
 
  
  
- 
- 
-}
+});
